f8440cc3f03f5e5506328ca1effd896383f891b2
satrik <dev@petrik.de>
2024-02-22 14:22:47 +0100

    test

--- Casks/testing.rb
+++ Casks/testing.rb
@@ -1,6 +1,6 @@
 cask "testing" do
   version "0.1"
-  sha256 "1ad7b7222c675bd7a4be7a976e0caef98c839d014ab539733091a813873c0917"
+  sha256 "c87621e77683dd9cc1853f52a824b2f1eb4594383299967028918cc32aa5d213"
 
   url "https://github.com/satrik/testing/releases/download/#{version}/testing.dmg"
   name "testing"
@@ -10,5 +10,6 @@ cask "testing" do
   depends_on macos: ">= :mojave"
 
   app "testing.app"
-
+  
 end
+
