49568a2a9530f9946093e5f0e563c919657a05ae
satrik <dev@petrik.de>
2024-02-22 14:26:38 +0100

    test

--- Casks/testing.rb
+++ Casks/testing.rb
@@ -1,6 +1,6 @@
 cask "testing" do
   version "0.1"
-  sha256 "c87621e77683dd9cc1853f52a824b2f1eb4594383299967028918cc32aa5d213"
+  sha256 "97de86279a93d519acc8dabbdd2c9aadb6682bbd7533a4edb5ef6ca4cdaa9125"
 
   url "https://github.com/satrik/testing/releases/download/#{version}/testing.dmg"
   name "testing"
