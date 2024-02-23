45a8b740f6a57b84e01b1faca02a21aa0625de46
Oluf Lorenzen <fink.github.com@mafia-server.net>
2024-02-23 21:42:34 +0100

    Update testing.rb

--- Casks/testing.rb
+++ Casks/testing.rb
@@ -3,14 +3,14 @@ cask "testing" do
   version "1.5"
   sha256 "d43e85599d5f3b9213dffdc4be831f09867754c4f3a51b7f94042bd9a41fbef2"
 
-  url "https://github.com/satrik/testing/releases/download/#{version}/testing.dmg"
+  url "https://github.com/satrik/testing/releases/download/#{version}/toggleMute.dmg"
   name "testing"
   desc "testing desc"
   homepage "https://github.com/satrik/testing"
 
   livecheck do
-    url "https://github.com/satrik/testing/releases/download/#{version}/testing.dmg"
-    regex(/testing.dmg/i)
+    url "https://github.com/satrik/testing/releases/download/#{version}/toggleMute.dmg"
+    regex(/toggleMute.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
@@ -18,6 +18,6 @@ cask "testing" do
 
   depends_on macos: ">= :mojave"
 
-  app "testing.app"
+  app "toggleMute.app"
 
 end
